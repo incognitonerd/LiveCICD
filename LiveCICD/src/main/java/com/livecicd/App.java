e333fd62fa40c89dce45545d5aa8a1c371d7f40a
Argos <Argos@10.0.0.9>
2017-12-03 17:55:00 -0500

    testing jenkins auto build 2

--- LiveCICD/src/main/java/com/livecicd/App.java
+++ LiveCICD/src/main/java/com/livecicd/App.java
@@ -4,5 +4,6 @@ public class App {
 	public static void main(String[] args){
 		System.out.println("hello world");
 		System.out.println("testing jenkins auto build");
+		System.out.println("testing jenkins auto build 2");
 	}
 }
