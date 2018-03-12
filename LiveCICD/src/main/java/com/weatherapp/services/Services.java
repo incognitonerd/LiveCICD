9b84abc5ff0f0e734d3f592e87ca124cd86cdbc9
incognitonerd <Argos@10.0.0.9>
2018-03-12 06:04:06 -0400

    added exception handling for 500 error

--- LiveCICD/src/main/java/com/weatherapp/services/Services.java
+++ LiveCICD/src/main/java/com/weatherapp/services/Services.java
@@ -1,8 +1,10 @@
 package com.weatherapp.services;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.weatherapp.attributes.Results;
 import com.weatherapp.responseDTOs.ErrorMsgs;
 import com.weatherapp.responseDTOs.ResponseMsgs;
@@ -12,18 +14,25 @@ import com.weatherapp.utilities.Utilities;
 @RestController
 @RequestMapping(value = "/zaysServices")
 public class Services {
-	private static final String errMsg = "Invalid Zip Code!";
-	
+	private static final String invalidZipCodeError = "Invalid Zip Code!";
+	private static final String queryLimitError = "OVER_QUERY_LIMIT!";
+
 	// example request: http://localhost:8080/zaysServices/weatherApp?zipCode=30032
 	@RequestMapping(value = "/weatherApp", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
-	public Object weatherApp(@RequestParam(value = "zipCode", required = true) String zipCode) throws Exception{
-		if(Utilities.validateZip(zipCode)){
-			Results result = Utilities.fetchLatAndLong(zipCode);
-			WeatherResponse wr = Utilities.fetchWeather(result.getGeometry().getLocation());
-			return new ResponseMsgs(Utilities.getTime(wr.getCurrently().getTime()), result.getFormatted_address(), wr
-					.getCurrently().getTemperature(), wr.getCurrently().getSummary());
-		} else{
-			return new ErrorMsgs(errMsg);
+	public Object weatherApp(@RequestParam(value = "zipCode", required = true) String zipCode) throws Exception {
+		Results result = null;
+		WeatherResponse wr = null;
+		if (Utilities.validateZip(zipCode)) {
+			try {
+				result = Utilities.fetchLatAndLong(zipCode);
+				wr = Utilities.fetchWeather(result.getGeometry().getLocation());
+				return new ResponseMsgs(Utilities.getTime(wr.getCurrently().getTime()), result.getFormatted_address(),
+						wr.getCurrently().getTemperature(), wr.getCurrently().getSummary());
+			} catch (Exception e) {
+				return new ErrorMsgs(queryLimitError);
+			}
+		} else {
+			return new ErrorMsgs(invalidZipCodeError);
 		}
 	}
 }
--- LiveCICD/src/main/java/com/weatherapp/utilities/Utilities.java
+++ LiveCICD/src/main/java/com/weatherapp/utilities/Utilities.java
@@ -1,13 +1,16 @@
 package com.weatherapp.utilities;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
 import java.util.Properties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
+
 import com.weatherapp.attributes.Location;
 import com.weatherapp.attributes.Results;
 import com.weatherapp.responseDTOs.WeatherResponse;
@@ -15,81 +18,91 @@ import com.weatherapp.responseDTOs.ZipCodeResponse;
 
 public class Utilities {
 	private static final Logger log = LoggerFactory.getLogger(Utilities.class);
-	
-	public static boolean validateZip(String zipCode){
-		if(zipCode.length() != 5)
+
+	public static boolean validateZip(String zipCode) {
+		if (zipCode.length() != 5)
 			return false;
-		try{
+		try {
 			int result = Integer.parseInt(zipCode);
-		} catch(Exception e){
+		} catch (Exception e) {
 			log.info("failed to parse zipCode!");
 			return false;
 		}
 		return true;
 	}
-	
-	public static String getTime(Long time){
+
+	public static String getTime(Long time) {
 		Date d = new Date(time * 1000);
 		log.info(d.toString());
 		return d.toString();
 	}
-	
-	public static Results fetchLatAndLong(String zipCode){
-		String url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + zipCode;
-		RestTemplate rt = new RestTemplate();
+
+	public static Results fetchLatAndLong(String zipCode) {
+		RestTemplate rt = null;
 		ZipCodeResponse response = null;
-		Boolean lock = true;
-		while(lock){
-			response = rt.getForObject(url, ZipCodeResponse.class);
-			if(response != null){
-				lock = false;
+		try {
+			String url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + zipCode;
+			rt = new RestTemplate();
+			Boolean lock = true;
+			while (lock) {
+				response = rt.getForObject(url, ZipCodeResponse.class);
+				if (response != null) {
+					lock = false;
+				}
 			}
-		}
-		log.info("Status : " + response.getStatus());
-		if(response.getStatus().equals("OK")){
-			for(Results result: response.getResults()){
-				return result;
+			log.info("Status : " + response.getStatus());
+			if (response.getStatus().equals("OK")) {
+				for (Results result : response.getResults()) {
+					return result;
+				}
 			}
+		} catch (Exception e) {
+			return null;
 		}
 		return null;
 	}
-	
-	// you must create a darksky account in order to obtain an apiKey to use their services
-	public static WeatherResponse fetchWeather(Location loc) throws IOException{
-		// create a darksky account and replace this api key with your key
-		String apiKey = getApiKey();
-		String url = "https://api.darksky.net/forecast/" + apiKey + "/" + loc.getLat() + "," + loc.getLng();
-		RestTemplate rt = new RestTemplate();
+
+	// you must create a darksky account in order to obtain an apiKey to use their
+	// services
+	public static WeatherResponse fetchWeather(Location loc) throws IOException {
 		WeatherResponse wr = null;
-		Boolean lock = true;
-		while(lock){
-			wr = rt.getForObject(url, WeatherResponse.class);
-			if(wr != null){
-				lock = false;
+		try {
+			// create a darksky account and replace this api key with your key
+			String apiKey = getApiKey();
+			String url = "https://api.darksky.net/forecast/" + apiKey + "/" + loc.getLat() + "," + loc.getLng();
+			RestTemplate rt = new RestTemplate();
+			Boolean lock = true;
+			while (lock) {
+				wr = rt.getForObject(url, WeatherResponse.class);
+				if (wr != null) {
+					lock = false;
+				}
 			}
+			log.info(wr.toString());
+			return wr;
+		} catch (Exception e) {
+			return wr;
 		}
-		log.info(wr.toString());
-		return wr;
 	}
-	
+
 	@SuppressWarnings("unused")
-	public static String getApiKey() throws IOException{
+	public static String getApiKey() throws IOException {
 		String apiKey = "";
 		InputStream configFile = null;
-		try{
+		try {
 			Properties prop = new Properties();
-			// configFile = new FileInputStream("//Users//Argos//Desktop//darkSkyApiKey.properties");
-			configFile = new FileInputStream("//var//darkSkyApiKey.properties");
-			if(configFile != null){
+			//configFile = new FileInputStream("//Users//Argos//Desktop//darkSkyApiKey.properties");
+			 configFile = new FileInputStream("//var//darkSkyApiKey.properties");
+			if (configFile != null) {
 				prop.load(configFile);
-			} else{
+			} else {
 				throw new FileNotFoundException("property file '" + configFile + "' not found in the classpath");
 			}
 			apiKey = prop.getProperty("apiKey");
 			log.info("Api Key - " + apiKey);
-		} catch(Exception e){
+		} catch (Exception e) {
 			log.info("Exception: " + e);
-		} finally{
+		} finally {
 			configFile.close();
 		}
 		return apiKey;
